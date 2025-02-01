695e9111b182ad6aad04124664bc73519ce5f59d
jeric celoso <jericceloso1@gmail.com>
2025-02-01 16:53:49 +0800

    all

---
+++ ToDo.List/src/main/java/com/todoapp/Main.java
@@ -0,0 +1,7 @@
+package com.todoapp;
+
+public class Main {
+	public static void main(String[] args ) {
+		
+	}
+}
---
+++ ToDo.List/src/main/java/com/todoapp/MainTask.java
@@ -0,0 +1,14 @@
+package com.todoapp;
+
+import com.todoapp.manager.TaskManager;
+
+public class MainTask {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TaskManager taskManager = new TaskManager();
+		
+		taskManager.MenuTask();
+	}
+
+}
